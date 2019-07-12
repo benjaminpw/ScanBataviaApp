f7079b82f083e2c55464f83ed67f422ff60e12c4
Eldrian <andieldrian@gmail.com>
2019-07-13 05:51:20 +0700

    Initial Commit

---
+++ JakartaLearningProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JakartaLearningProject
+//
+//  Created by Andi Ikhsan Eldrian on 13/07/19.
+//  Copyright © 2019 Andi Ikhsan Eldrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
