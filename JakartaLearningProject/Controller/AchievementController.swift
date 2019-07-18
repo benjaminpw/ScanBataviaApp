13a8ba86d6786647b7d345a8a86d2e9bfcab6374
Eldrian <andieldrian@gmail.com>
2019-07-18 16:03:33 +0700

    Collection View Added

--- JakartaLearningProject/Controller/AchievementController.swift
+++ JakartaLearningProject/Controller/AchievementController.swift
@@ -1,29 +1,54 @@
 //
-//  AchievementController.swift
+//  ExploreViewController.swift
 //  JakartaLearningProject
 //
-//  Created by Andi Ikhsan Eldrian on 13/07/19.
+//  Created by Steven Gunawan on 18/07/19.
 //  Copyright © 2019 Andi Ikhsan Eldrian. All rights reserved.
 //
 
 import UIKit
 
-class AchievementController: UIViewController {//, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
-
-
+class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+        let  image = images[indexPath.item]
+        cell.imageView.image = image
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfColumns: CGFloat = 2
+        let width = collectionView.frame.size.width
+        let xInsets: CGFloat = 10
+        let cellSpacing: CGFloat = 5
+        
+        return CGSize(width: (width / numberOfColumns) - (xInsets + cellSpacing), height: (width / numberOfColumns) - (xInsets + cellSpacing))
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let images: [UIImage] = [
+        UIImage(named: "0")!,
+        UIImage(named: "1")!,
+        UIImage(named: "2")!,
+        UIImage(named: "3")!,
+        UIImage(named: "4")!,
+        UIImage(named: "5")!,
+        UIImage(named: "6")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-//    //number of views
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    //populate views
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        return
-//    }
-
+    
+    
+    
 }
---
+++ JakartaLearningProject/View/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  JakartaLearningProject
+//
+//  Created by Steven Gunawan on 18/07/19.
+//  Copyright © 2019 Andi Ikhsan Eldrian. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
